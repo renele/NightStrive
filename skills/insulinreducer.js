2fc42a93bbafe96010f9ea3c28cef00c2c7ab9e4
René Klose <rene.klose@gmail.com>
2019-06-12 21:53:29 +0200

    Create insulinreducer.js

---
+++ skills/insulinreducer.js
@@ -0,0 +1,30 @@
+var mongoClient = require("mongodb").MongoClient;
+var server = "mongodb://heroku_06f8l08c:nfr62mmhsa9vg04n3vovlo9eb7@ds145474.mlab.com:45474/heroku_06f8l08c";
+var oldinsulin;
+var today = new Date().toISOString();
+
+module.exports = function (addedinsulin) {
+
+mongoClient.connect(server, { useNewUrlParser: true }, function(error, db)  { 
+if(error)
+console.log("Error while connecting to database: ", error);
+else
+//console.log("Connection established successfully");
+    //perform operations here
+
+    var dbo = db.db("heroku_06f8l08c");
+    dbo.collection("insulin").findOne({}, {sort:{$natural:-1}}, function(err, result) {
+      if (err) throw err;
+      console.log(result.insulinleft);
+      oldinsulin = result.insulinleft;
+      var remaininginsulin = oldinsulin - addedinsulin ;
+      var myobj = { type: "Insulin", insulin: "Humalog", insulinleft: remaininginsulin, date: today, };
+      dbo.collection("insulin").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+      
+    });
+    });
+  });
+}
