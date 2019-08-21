c1b2ad70315547ab1fad578005cf3e03dab57af4
René Klose <rene.klose@gmail.com>
2019-08-21 18:26:46 +0200

    Update nasa.js

--- skills/nasa.js
+++ skills/nasa.js
@@ -1,16 +1,16 @@
-var request = require("request");
-var options = { method: 'GET',
-  url: 'https://api.nasa.gov/planetary/apod',
-  qs: { api_key: '2TmpjkT9PSZhCM7HtFzblAjmy6eW3V0H6qJvfCHi' },
-  json: true,
-  headers:  { } };
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+//var request = require("request");
+//var options = { method: 'GET',
+//  url: 'https://api.nasa.gov/planetary/apod',
+//  qs: { api_key: '2TmpjkT9PSZhCM7HtFzblAjmy6eW3V0H6qJvfCHi' },
+//  json: true,
+//  headers:  { } };
+// request(options, function (error, response, body) {
+//  if (error) throw new Error(error);
 
  // console.log(response.body.explanation);
  // console.log(" ");
  // console.log(response.body.hdurl)
-});
+//});
 
 
 
